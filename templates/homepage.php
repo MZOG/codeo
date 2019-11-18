b0252d377ab3157924e2e5cd8f5e302a0c592d62
Marcin Zogrodnik <marcin.zogrodnik@deeperthanblue.co.uk>
2019-11-18 23:46:26 +0000

    styling portfolio section

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,8 +1,48 @@
 <?php /* Template Name: Homepage */ get_header();?>
 
+<?php
+ $portfolio = new WP_Query( array(
+  'category_name' => 'portfolio',
+  'posts_per_page' => 2,
+));
+?>
+
 <!-- intro -->
 <section class="intro">
+  <div class="container ">
+    <p>hejka</p>
+  </div>
+</section>
+
+
+<!-- portfolio -->
+<section class="portfolio">
   <div class="container">
+    <h2 class="noe">Realizacje</h2>
+    <p>Zobacz ostatnie realizacje wykonane przez nasz zespół.</p>
+
+    <div class="portfolio-container">
+    <?php if ( $portfolio->have_posts() ) : ?>
+    <?php while ( $portfolio->have_posts() ) : $portfolio->the_post();?>
+      <article class="portfolio-item">
+          <div class="portfolio-image">
+              <a href="<?php echo get_permalink(); ?>">
+                  <img src="<?php echo the_post_thumbnail_url( 'full' ); ?>" alt="<?php the_title(); ?>">
+              </a>
+          </div>
+          <div class="more"><a class="noe" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></div>
+      </article>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+    <?php else : ?>
+        <p><?php __('No items'); ?></p>
+    <?php endif; ?>
+    </div>
+
+    <div class="portfolio-cta">
+      <a href="#" class="btn">Zobacz więcej</a>
+    </div>
+
   </div>
 </section>
 
